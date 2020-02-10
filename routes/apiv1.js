cc35051e3b226798af332b7640eb8210990c2523
Juniardi Akbar <juniardiakbar@gmail.com>
2020-02-10 22:31:36 +0700

    [INC] Implement sign in/out, add libraries

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const { isAuthenticated, isNonAuthenticated } = require('../middlewares/user');
+const { signInSSO, postSignout } = require('../handlers/user');
 
 const r = express.Router();
 
-r.get('/', (req, res) =>
+r.get('/', isAuthenticated, (req, res) => {
   res.json({
     apiVersion: res.locals.apiVersion,
     message: 'Server is up and running'
-  })
-);
+  });
+});
+
+r.get('/auth/signin', isNonAuthenticated, signInSSO);
+r.get('/auth/signout', isAuthenticated, postSignout);
+r.get('/auth/check', isAuthenticated, (req, res) => {
+  const { user } = req.session;
+  res.json(user);
+});
 
 module.exports = r;
