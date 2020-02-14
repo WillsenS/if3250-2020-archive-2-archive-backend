0565efd621307929409e2e0a6ba91a9e3b173046
hendpraz <mhendryp99@gmail.com>
2020-02-14 22:34:18 +0700

    Add .env to deployment script

--- deploy/updateAndRestart.sh
+++ deploy/updateAndRestart.sh
@@ -1,35 +1,24 @@
-#!/bin/bash
-
 set -e
 
-# Delete the old repository folder
 sudo su
-rm -rf /home/ubuntu/archive-backend/
 
-# Clone the github repository staging branch (usually develop)
-cd /home/ubuntu/
-git clone -b task-9/ci-cd --single-branch git@gitlab.informatika.org:if3250-2020-archive-2/archive-backend.git
-
-# Create .env variables
+chmod 755 /home/ubuntu/archive-backend/
 cd /home/ubuntu/archive-backend/
-echo MONGODB_URI=$MONGODB_URI >> .env
-echo BASE_URL=$BASE_URL >> .env
-echo SESSION_SECRET=$SESSION_SECRET >> .env
 
 # Stop the previous pm2
-# echo "Killing previous pm2 and removing pm2..."
-# pm2 kill
-# npm remove pm2 -g
+echo "Killing previous pm2 and removing pm2..."
+pm2 kill
+npm remove pm2 -g
 
 # Install pm2 and run pm2 daemon
-# echo "Installing pm2 globally..."
-# npm install pm2 -g
-# pm2 status
+echo "Installing pm2 globally..."
+npm install pm2 -g
+pm2 status
 
 # Install npm packages
-# echo "Running npm install -g"
-# npm install -g
+echo "Running npm install -g"
+npm install -g
 
-#Restart the node
-# echo "Restarting node..."
-# pm2 start app.js
+Restart the node
+echo "Restarting node..."
+pm2 start app.js
