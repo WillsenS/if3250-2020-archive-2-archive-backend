cf4e48477aa1c5a95cf55032a3e5126e17dc8372
hendpraz <mhendryp99@gmail.com>
2020-02-15 08:46:53 +0700

    Update deployment script to only integrate develop branch

--- deploy/clone.sh
+++ deploy/clone.sh
@@ -8,6 +8,6 @@ rm -rf /home/ubuntu/archive-backend/
 
 # Clone the github repository staging branch (usually develop)
 cd /home/ubuntu/
-git clone -b task-9/ci-cd --single-branch git@gitlab.informatika.org:if3250-2020-archive-2/archive-backend.git
+git clone -b develop --single-branch git@gitlab.informatika.org:if3250-2020-archive-2/archive-backend.git
 
 chmod 777 /home/ubuntu/archive-backend/
