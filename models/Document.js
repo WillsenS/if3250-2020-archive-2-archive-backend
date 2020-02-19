961b3d9a86f5ba07f78ab9049e874cfcf41e4554
Juniardi Akbar <juniardiakbar@gmail.com>
2020-02-19 16:46:19 +0700

    Add seeders

---
+++ models/Document.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const documentSchema = new mongoose.Schema(
+  {
+    kode: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    judul: {
+      type: String,
+      required: true
+    },
+    keterangan: {
+      type: String,
+      required: true
+    },
+    lokasi: {
+      type: String,
+      required: true
+    },
+    file: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'File'
+    }
+  },
+  { timestamps: true }
+);
+
+const Document = mongoose.model('Document', documentSchema);
+
+module.exports = Document;
