46f1da0159659dd53d7d420af5dbc1c46316e70f
Juniardi Akbar <juniardiakbar@gmail.com>
2020-02-29 13:19:45 +0700

    Implement search filter

--- handlers/document.js
+++ handlers/document.js
@@ -1,13 +1,14 @@
 const Document = require('../models/Document');
+const { translateFiltersMongoose } = require('../helpers');
 
 exports.searchDocument = async (req, res) => {
-  let { q, page } = req.query;
+  let { q, page, filters } = req.query;
 
-  page = parseInt(page, 10) > 0 ? parseInt(page, 10) : 1;
   q = q || '';
+  page = parseInt(page, 10) > 0 ? parseInt(page, 10) : 1;
 
-  const limit = 2;
-  const options = {
+  const limit = 5;
+  let where = {
     $text: {
       $search: q
         .split(' ')
@@ -15,20 +16,50 @@ exports.searchDocument = async (req, res) => {
         .join(' ')
     }
   };
-  const countDocument = await Document.countDocuments(options);
-  const findDocument = await Document.find(options)
+
+  if (filters) {
+    options = translateFiltersMongoose(filters);
+    where = { $and: [where, options] };
+  }
+
+  const countDocument = await Document.countDocuments(where);
+  const findDocument = await Document.find(where)
     .limit(limit)
     .skip((page - 1) * limit);
 
-  const baseLink = `${process.env.BASE_URL}/api/v1/search?q=${q}`;
-  const nextLink = countDocument / (page * limit) > 1 ? `${baseLink}&page=${page + 1}` : null;
-  const prevLink = page !== 1 ? `${baseLink}&page=${page - 1}` : null;
+  let qs = '?';
+  const qsNameList = ['q', 'filters'];
+  [q, filters].forEach((val, index) => {
+    if (qs === '?') {
+      if (val) qs += `${qsNameList[index]}=${val}`;
+    } else if (val) qs += `&${qsNameList[index]}=${val}`;
+  });
+
+  const totalPages = Math.ceil(countDocument / limit);
+  const baseLink = `${process.env.BASE_URL}/api/v1/search`;
+  const nextLink = totalPages > page ? `${baseLink}${qs}&page=${page + 1}` : '#';
+  const prevLink = page > 1 ? `${baseLink}${qs}&page=${page - 1}` : '#';
+
+  const filterAttr = ['lokasi'];
+  const filtersCandidate = [];
+  filterAttr.forEach(val => {
+    let arr = [];
+
+    findDocument.forEach(doc => {
+      if (!arr.includes(doc[val])) arr.push(doc[val]);
+    });
+
+    const obj = {};
+    obj[val] = arr;
+    filtersCandidate.push(obj);
+  });
 
   res.json({
     data: findDocument,
     count: countDocument,
     page,
-    next_link: nextLink,
-    prev_link: prevLink
+    filtersCandidate,
+    nextLink: nextLink,
+    prevLink: prevLink
   });
 };
---
+++ helpers/stringparser.js
@@ -0,0 +1,105 @@
+const operators = require('./operators.js');
+
+/**
+ * Translate query string of filter into mongoose query operator object
+ * @param filterString String that matches with google standard string query string
+ * that will be converted into mongoose query operator.
+ */
+exports.translateMongooseFilter = filterString => {
+  const { list } = operators;
+  let result = null;
+
+  list.forEach(val => {
+    if (filterString.includes(val)) {
+      const formula = filterString.split(val);
+      const formulaObject = {};
+      switch (val) {
+        case '==': {
+          formulaObject[formula[0]] = {
+            $eq: formula[1]
+          };
+          result = formulaObject;
+          return result;
+        }
+        case '!=': {
+          formulaObject[formula[0]] = {
+            $ne: formula[1]
+          };
+          result = formulaObject;
+          return result;
+        }
+        case '<': {
+          formulaObject[formula[0]] = {
+            $lt: formula[1]
+          };
+          result = formulaObject;
+          return result;
+        }
+        case '>': {
+          formulaObject[formula[0]] = {
+            $gt: formula[1]
+          };
+          result = formulaObject;
+          return result;
+        }
+        case '<=': {
+          formulaObject[formula[0]] = {
+            $lte: formula[1]
+          };
+          result = formulaObject;
+          return result;
+        }
+        case '>=': {
+          formulaObject[formula[0]] = {
+            $gte: formula[1]
+          };
+          result = formulaObject;
+          return result;
+        }
+        case '=@': {
+          formulaObject[formula[0]] = {
+            $regex: formula[1],
+            $options: 'i'
+          };
+          result = formulaObject;
+          return result;
+        }
+        case '!@': {
+          formulaObject[formula[0]] = {
+            $not: {
+              $ne: formula[1]
+            }
+          };
+          result = formulaObject;
+          return result;
+        }
+        default:
+          return result;
+      }
+    }
+  });
+  return result;
+};
+
+/**
+ * Match query string based on Google Filters Standard
+ * @param filtersQueryString
+ */
+exports.translateFiltersMongoose = filtersQueryString => {
+  const orArray = filtersQueryString.split(',').map(val => {
+    return val.split(';');
+  });
+
+  const filterArray = orArray.map(andArray => {
+    const translatedArray = andArray.map(val => {
+      return this.translateMongooseFilter(val);
+    });
+    return {
+      $and: translatedArray || []
+    };
+  });
+
+  return {
+    $or: filterArray
+  };
+};
