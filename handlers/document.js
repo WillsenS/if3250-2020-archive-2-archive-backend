ec768a070d806acd8723f75c21c6ff4444b5a2fe
hendpraz <mhendryp99@gmail.com>
2020-02-29 17:16:41 +0700

    Implement save model to db after uploaded an archive

--- handlers/document.js
+++ handlers/document.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-await-in-loop */
 const formidable = require('formidable');
 const mv = require('mv');
+const mongoose = require('mongoose');
 const Document = require('../models/Document');
 const { translateFiltersMongoose } = require('../helpers');
+const File = require('../models/File');
 
 exports.searchDocument = async (req, res) => {
   try {
@@ -73,10 +75,49 @@ exports.searchDocument = async (req, res) => {
   }
 };
 
+const connect = new Promise((resolve, reject) => {
+  mongoose.connect(process.env.MONGODB_URI, {
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true,
+    useNewUrlParser: true
+  });
+  mongoose.connection.on('error', err => {
+    reject(err);
+  });
+  resolve('Mongoose is connected');
+});
+
+/* eslint-disable */
+const saveModel = (connector, Resource, data) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      const connection = await connector;
+      if (connection) {
+        const promiseArray = data.map(item => {
+          const newItem = new Resource(item);
+          return newItem.save();
+        });
+
+        const result = await Promise.all(promiseArray);
+        resolve(result);
+      }
+      throw new Error('Connection could not be established');
+    } catch (e) {
+      reject(e);
+    }
+  });
+
+/* eslint-enable */
+
 exports.uploadArchive = (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<form action="upload" method="post" enctype="multipart/form-data">');
-  res.write('<input type="file" name="filetoupload"><br>');
+  res.write('<div>Judul: <input type="text" name="title"></div><br>');
+  res.write('<div>File: <input type="file" name="filetoupload"></div><br>');
+  res.write('<div>Kode: <input type="text" name="code"></div><br>');
+  res.write('<div>Description: <input type="text" name="description"></div><br>');
+  res.write('<div>Location: <input type="text" name="location"></div><br>');
   res.write('<input type="submit">');
   res.write('</form>');
   return res.end();
@@ -84,9 +125,48 @@ exports.uploadArchive = (req, res) => {
 
 exports.postUploadArchive = async (req, res) => {
   const form = new formidable.IncomingForm();
-  form.parse(req, function(err, fields, files) {
+
+  form.parse(req, async function(err, fields, files) {
     const oldpath = files.filetoupload.path;
-    const newpath = process.env.NODE_PATH + process.env.UPLOAD_DIR + files.filetoupload.name;
+    const newpath =
+      process.env.NODE_PATH +
+      process.env.PUBLIC_DIR +
+      process.env.UPLOAD_DIR +
+      files.filetoupload.name;
+
+    const data = [
+      {
+        originalname: files.filetoupload.name,
+        encoding: 'undefined-encoding',
+        filename: files.filetoupload.name,
+        mimetype: files.filetoupload.type,
+        size: files.filetoupload.size,
+        path: process.env.UPLOAD_DIR + files.filetoupload.name
+      }
+    ];
+
+    console.log(data);
+
+    const result = await saveModel(connect, File, data);
+    console.info(result);
+
+    /* eslint-disable */
+    const dataDocument = [
+      {
+        kode: fields.code,
+        judul: fields.title,
+        keterangan: fields.description,
+        lokasi: fields.location,
+        file: result[0]._id
+      }
+    ];
+    /* eslint-enable */
+
+    console.log(dataDocument);
+
+    const resultDocument = await saveModel(connect, Document, dataDocument);
+    console.info(resultDocument);
+
     mv(oldpath, newpath, function() {
       res.json({
         apiVersion: res.locals.apiVersion,
