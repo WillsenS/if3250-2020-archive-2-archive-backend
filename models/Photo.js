2c62f9a1e616170f07f242b713e7b4b61439c894
Willsen <13517036@std.stei.itb.ac.id>
2020-03-31 09:01:39 +0700

    add mongoose

---
+++ models/Photo.js
@@ -0,0 +1,89 @@
+const mongoose = require('mongoose');
+// eslint-disable-next-line no-unused-vars
+const File = require('../models/File');
+
+/**
+ * Document Interface
+ * @typedef {Object} Document
+ * @property {String} kode Kode dari arsip
+ * @property {String} judul Judul dari arsip
+ * @property {String} keterangan Keterangan dari arsip
+ * @property {String} lokasi Lokasi arsip
+ * @property {String} file File arsip
+ */
+const documentSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: false
+    },
+    type: {
+      type: String,
+      required: true,
+      unique: false
+    },
+    judul: {
+      type: String,
+      required: true
+    },
+    code: {
+      type: String,
+      required: true
+    },
+    location: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: Date,
+      required: true
+    },
+    dateUploaded: {
+      type: Date,
+      required: true
+    },
+    archiveLocation: {
+      type: String,
+      required: true
+    },
+    mime: {
+      type: String,
+      required: true
+    },
+    photographer: {
+      type: String,
+      required: true
+    },
+    photoSize: {
+      type: String,
+      required: true
+    },
+    photoCondition: {
+      type: String,
+      required: true
+    },
+    hak_akses: {
+      type: [String],
+      required: true
+    },
+    unit_kerja_terkait: {
+      type: [String],
+      required: true
+    },
+    file: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'File'
+    }
+  },
+  { timestamps: true }
+);
+
+documentSchema.index({ judul: 'photo', keterangan: 'photo' });
+const Document = mongoose.model('Photo', documentSchema);
+
+module.exports = Document;
