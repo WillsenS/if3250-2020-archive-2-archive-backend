3592b2de8d590b6c3d3a223f0c3695ecbf2338ea
Juniardi Akbar <juniardiakbar@gmail.com>
2020-04-13 23:41:14 +0700

    Fix download file

--- handlers/archive.js
+++ handlers/archive.js
@@ -31,7 +31,7 @@ const isValid = async user => {
 exports.isAuthArchive = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const foundArchive = File.findById(id);
+    const foundArchive = await Archive.findById(id);
 
     if (!foundArchive.keamanan_terbuka) {
       const bearerHeader = req.headers.authorization;
@@ -307,26 +307,26 @@ exports.getArchiveDetail = async (req, res) => {
 exports.downloadArchive = async (req, res) => {
   const { id } = req.params;
   try {
-    const foundArchive = await Archive.findById(id);
-    const result = await File.findById(foundArchive.file);
-    const file = process.env.NODE_PATH +
-    process.env.PUBLIC_DIR +
-    process.env.UPLOAD_DIR +
-    result.filename;
-    res.download(file, function (err) {
+    const foundArchive = await Archive.findById(id).populated('file');
+
+    const file =
+      process.env.NODE_PATH +
+      process.env.PUBLIC_DIR +
+      process.env.UPLOAD_DIR +
+      foundArchive.file.filename;
+
+    return res.download(file, err => {
       if (err) {
         console.error(err);
         return sendResponse(res, 400, 'Error. Bad request');
-      } else {
-        return sendResponse(res, 200, 'Successfully downloaded');
       }
+      return sendResponse(res, 200, 'Successfully downloaded');
     });
   } catch (err) {
     console.error(err);
     return sendResponse(res, 400, 'Error. Bad request');
   }
-  
-}
+};
 
 const buildArchiveFromForm = async (req, res) => {
   const form = new formidable.IncomingForm();
