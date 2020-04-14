2fa5064e684057cce3bf3d20c2e3934e3f7c8158
hendpraz <mhendryp99@gmail.com>
2020-04-14 10:00:59 +0700

    Add Admin Role Checker Middlewares

--- middlewares/user.js
+++ middlewares/user.js
@@ -2,6 +2,7 @@ const secret = 'mysecretsshhh';
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
+const { sendResponse } = require('../helpers');
 
 const isValid = async user => {
   const { _id } = user;
@@ -72,4 +73,97 @@ exports.isNonAuthenticated = (req, res, next) => {
       message: 'You are not allowed.'
     }
   });
+};
+
+const HIGHEST_ADMIN_REQUEST = 'highest';
+const ADMIN_ALL_LEVEL_REQUEST = 'all_level';
+const ADMIN_ONLY_REQUEST = 'admin_only';
+
+const HIGHEST_ADMIN_ROLE = 1; // Admin Terpusat
+const MINIMUM_ADMIN_ROLE = 3; // Minimum value of Admin Role Code
+
+const bearerChecker = async (req, code) => {
+  const bearerHeader = req.headers.authorization;
+
+  if (bearerHeader) {
+    const bearer = bearerHeader.split(' ');
+    const bearerToken = bearer[1];
+
+    const decode = jwt.verify(bearerToken, secret);
+    const valid = await isValid(decode.user);
+
+    if (decode.user && valid) {
+      req.session.user = decode.user;
+      let isRoleValid = false;
+      const { user } = decode;
+
+      const { _id } = user;
+      const foundUser = await User.findById(_id);
+
+      switch (code) {
+        case HIGHEST_ADMIN_REQUEST:
+          isRoleValid = user.role === HIGHEST_ADMIN_ROLE && foundUser.role === user.role;
+          break;
+        case ADMIN_ALL_LEVEL_REQUEST:
+          isRoleValid =
+            (user.role === HIGHEST_ADMIN_ROLE || user.role >= MINIMUM_ADMIN_ROLE) &&
+            foundUser.role === user.role;
+          break;
+        case ADMIN_ONLY_REQUEST:
+          isRoleValid = user.role >= MINIMUM_ADMIN_ROLE && foundUser.role === user.role;
+          break;
+        default:
+          throw new Error('Invalid request code');
+      }
+
+      return isRoleValid;
+    }
+  }
+
+  return false;
+};
+
+exports.isHighestAdmin = async (req, res, next) => {
+  try {
+    const result = await bearerChecker(req, HIGHEST_ADMIN_REQUEST);
+
+    if (result) {
+      return next();
+    }
+
+    return sendResponse(res, 401, 'Error. You are not allowed');
+  } catch (e) {
+    console.error(e);
+    return sendResponse(res, 500, `Error: ${e}`);
+  }
+};
+
+exports.isAdminAllLevel = async (req, res, next) => {
+  try {
+    const result = await bearerChecker(req, ADMIN_ALL_LEVEL_REQUEST);
+
+    if (result) {
+      return next();
+    }
+
+    return sendResponse(res, 401, 'Error. You are not allowed');
+  } catch (e) {
+    console.error(e);
+    return sendResponse(res, 500, `Error: ${e}`);
+  }
+};
+
+exports.isAdminOnly = async (req, res, next) => {
+  try {
+    const result = await bearerChecker(req, ADMIN_ONLY_REQUEST);
+
+    if (result) {
+      return next();
+    }
+
+    return sendResponse(res, 401, 'Error. You are not allowed');
+  } catch (e) {
+    console.error(e);
+    return sendResponse(res, 500, `Error: ${e}`);
+  }
 };
