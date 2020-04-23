3ab60c31a4653f1cd16e089af4923ed5866202ef
Muhammad Hendry Prasetya <13517105@std.stei.itb.ac.id>
2020-04-23 19:00:51 +0700

    Update config

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 const config = {
-  defaultURL: 'http://localhost:3000'
+  // defaultURL: 'http://localhost:3000'
   // defaultURL: 'https://aec87051.ngrok.io'
-  // defaultURL: 'https://demoapp.my.id'
+  defaultURL: 'https://demoapp.my.id'
 };
 
 module.exports = config;
